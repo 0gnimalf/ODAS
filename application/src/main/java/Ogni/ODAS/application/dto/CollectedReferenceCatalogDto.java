a3d495a402bead92bfb7095d5bf2f78522ba1428
0gnimalf <ognimalf.xd@gmail.com>
2026-04-14 13:44:58 +0300

    change unit

--- application/src/main/java/Ogni/ODAS/application/dto/CollectedReferenceCatalogDto.java
+++
@@ -1,9 +0,0 @@
-package Ogni.ODAS.application.dto;
-
-import java.util.List;
-
-public record CollectedReferenceCatalogDto(
-        List<CollectedRegionDto> regions,
-        List<CollectedIndicatorDto> indicators
-) {
-}
--- domain/src/main/java/Ogni/ODAS/domain/enumtype/ObservationValueKind.java
+++ domain/src/main/java/Ogni/ODAS/domain/enumtype/ObservationValueKind.java
@@ -7,22 +7,22 @@ public enum ObservationValueKind {
     // доходы | расходы | кредиты (сравнение по регионам) | источники финансирования (детально)
     PLAN(
             "План",
-            UnitCode.RUB, ObservationValueType.ABSOLUTE),
+            UnitCode.THOUSAND_RUB, ObservationValueType.ABSOLUTE),
     REFINED_PLAN_CONSOLIDATED_SUBJECT_BUDGET(
             "Уточненный план; конс.бюджет субъекта РФ",
-            UnitCode.RUB, ObservationValueType.ABSOLUTE),
+            UnitCode.THOUSAND_RUB, ObservationValueType.ABSOLUTE),
     REFINED_PLAN_SUBJECT_BUDGET(
             "Уточненный план; в т.ч.бюджет субъекта",
-            UnitCode.RUB, ObservationValueType.ABSOLUTE),
+            UnitCode.THOUSAND_RUB, ObservationValueType.ABSOLUTE),
     REFINED_PLAN_RATE_TO_PREVIOUS_PERIOD_EXECUTION(
             "Темп уточненного плана к исполнению пред.года,%",
             UnitCode.PERCENT, ObservationValueType.RATIO),
     ACTUAL_CONSOLIDATED_SUBJECT_BUDGET(
             "Исполнено;  конс.бюджет субъекта РФ",
-            UnitCode.RUB, ObservationValueType.ABSOLUTE),
+            UnitCode.THOUSAND_RUB, ObservationValueType.ABSOLUTE),
     ACTUAL_SUBJECT_BUDGET(
             "Исполнено; в т.ч.бюджет субъекта",
-            UnitCode.RUB, ObservationValueType.ABSOLUTE),
+            UnitCode.THOUSAND_RUB, ObservationValueType.ABSOLUTE),
     // (доходы | расходы) сравнение по регионам
     ACTUAL_RATE_TO_PREVIOUS_PERIOD(
             "Исполнено; Темп к соотв.периоду пред.года,%",
@@ -35,8 +35,11 @@ public enum ObservationValueKind {
             "Темп роста к соотв.периоду прошл.года,%; по ФО",
             UnitCode.PERCENT, ObservationValueType.RATIO),
     GROWTH_RATE_TO_PREVIOUS_PERIOD_BY_RUSSIAN_FEDERATION(
-            "Темп роста к соотв.периоду прошл.года,%; по РФ", UnitCode.PERCENT,
-            ObservationValueType.RATIO)
+            "Темп роста к соотв.периоду прошл.года,%; по РФ",
+            UnitCode.PERCENT, ObservationValueType.RATIO),
+    POPULATION(
+            "Численность населения (чел.)",
+            UnitCode.PERSON, ObservationValueType.ABSOLUTE),
     ;
 
     private final String label;
